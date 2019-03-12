deb5bcc6c5762179feea0060abc4520f77e9d377
Aurimas Vilčinskas <aurimas.vlcnks@gmail.com>
2019-03-12 22:08:09 +0200

    demo lib

---
+++ src/add.test.js
@@ -0,0 +1,11 @@
+import add from './add';
+
+describe('add', () => {
+    test('integer', () => {
+        expect(add(1, 2)).toStrictEqual(3);
+    });
+
+    test('failing test', () => {
+        expect(add(1, 3)).toStrictEqual(3);
+    });
+});
